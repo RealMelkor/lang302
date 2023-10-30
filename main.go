ed9ff8cb196e8706341f92d5ce768a66b5ea6991
Raw Monk <realmelkor@proton.me>
2023-10-30 15:43:16 -0400

    add multiple domains support

--- main.go
+++ main.go
@@ -11,10 +11,16 @@ import (
 	"os"
 )
 
-type config struct {
+type Domain struct {
+	Domain		string		`fig:"domain"`
 	Default		string		`fig:"default-language" default:"en"`
 	Languages	[]string	`fig:"languages" default:"[\"en\"]"`
 	RemoveRegion	bool		`fig:"remove-region"`
+}
+var Domains map[string]Domain
+
+type config struct {
+	Domains[] Domain
 	Network		struct {
 		Type	string	`fig:"type" default:"tcp"`
 		Port	int	`fig:"port" default:"9000"`
@@ -42,20 +48,27 @@ func contains(a []string, s string) bool {
 
 type FastCGIServer struct{}
 func (s FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	lang := cfg.Default
+	domain, ok := Domains[req.Host]
+	if !ok {
+		log.Println("unknown host:", req.Host)
+		w.WriteHeader(404)
+		w.Write([]byte("Invalid domain"))
+		return
+	}
+	lang := domain.Default
 	langs := strings.Split(req.Header.Get("Accept-Language"), ",")
 	for _, v := range langs {
 		s := strings.Split(v, ";")
 		if len(s) > 0 {
 			v = s[0]
 		}
-		if cfg.RemoveRegion {
+		if domain.RemoveRegion {
 			s = strings.Split(v, "-")
 			if len(s) > 0 {
 				v = s[0]
 			}
 		}
-		if contains(cfg.Languages, v) {
+		if contains(domain.Languages, v) {
 			lang = v
 			break
 		}
@@ -67,14 +80,20 @@ func (s FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 func main() {
 	if err := load(); err != nil {
 		log.Fatalln(err)
+		return
 	}
 	var listener net.Listener
+	if len(cfg.Domains) < 1 {
+		log.Fatalln("The domains list is empty")
+		return
+	}
 	if cfg.Network.Type == "tcp" {
 		addr := cfg.Network.Address + ":" +
 					strconv.Itoa(cfg.Network.Port)
 		l, err := net.Listen("tcp", addr)
 		if err != nil {
 			log.Fatalln(err)
+			return
 		}
 		listener = l
 		log.Println("Listening on", addr)
@@ -83,15 +102,22 @@ func main() {
 		unixAddr, err := net.ResolveUnixAddr("unix", cfg.Network.Unix)
 		if err != nil {
 			log.Fatalln(err)
+			return
 		}
 		l, err := net.ListenUnix("unix", unixAddr)
 		if err != nil {
 			log.Fatalln(err)
+			return
 		}
 		listener = l
 		log.Println("Listening on unix:" + cfg.Network.Unix)
 	} else {
 		log.Fatalln("invalid network type", cfg.Network.Type)
+		return
+	}
+	Domains = map[string]Domain{}
+	for _, v := range cfg.Domains {
+		Domains[v.Domain] = v
 	}
 	b := new(FastCGIServer)
         if err := fcgi.Serve(listener, b); err != nil {
